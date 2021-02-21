21ce0cb03bf7e48291dbe55b994c1405d32ce559
Timothy Exodus <exodustimothy@gmail.com>
2021-02-21 19:54:54 +0100

    global style - Color, Typography

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,28 @@
 </template>
 
 <style lang="scss">
+@import "./global-style/colors.scss";
+@import "./global-style/typography.scss";
+
+body {
+  background-color: $dark-blue;
+}
+
+h1 {
+  @include heading-1;
+}
+
+p {
+  @include large-text-bold ($purple);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: $white;
 }
 
 #nav {
@@ -22,7 +38,7 @@
 
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $white;
 
     &.router-link-exact-active {
       color: #42b983;
